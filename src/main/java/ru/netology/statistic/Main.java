8513c0606fad829a1401818480489d99289b5828
Ekaterina Romanova <Dekaterina83@mail.ru>
2024-11-07 15:44:56 +0300

    initial new project

---
+++ src/main/java/ru/netology/statistic/Main.java
@@ -0,0 +1,9 @@
+package ru.netology.statistic;
+
+public class Main {
+    public static void main(String[] args) {
+        StatisticsService service = new StatisticsService();
+        String[] incomesInBillions = new String[11];
+
+    }
+}
